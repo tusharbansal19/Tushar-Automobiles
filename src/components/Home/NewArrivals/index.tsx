34f04021425b49989e2304e5c9c17edc8256bd88
Tushar <tuharbansal3366@gmail.com>
2025-10-26 02:16:44 +0530

    Fix _id reference in NewArrivals component

--- src/components/Home/NewArrivals/index.tsx
+++ src/components/Home/NewArrivals/index.tsx
@@ -84,7 +84,7 @@ const NewArrival = () => {
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-7.5 gap-y-9">
             {/* <!-- New Auto Parts item --> */}
             {latestParts.map((item, key) => (
-              <ProductItem item={item as any} key={item._id || key} />
+              <ProductItem item={item as any} key={item.id || key} />
             ))}
           </div>
         ) : (
